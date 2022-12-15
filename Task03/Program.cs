78e101e5a54f6e4b178dd36465cdca3acbcb4087
Ваше имя английскими буквами <>
2022-12-15 22:55:10 +0300

    Добавили все задачи семинара

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*3. Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница*/
+
+Console.WriteLine("Ведите номер недели");
+string a = Console.ReadLine();
+if (a == "1") Console.WriteLine("Понедельник");
+
+else if (a == "2") Console.WriteLine("Вторник");
+
+else if (a == "3") Console.WriteLine("Среда");
+
+else if (a == "4") Console.WriteLine("Четверг");
+
+else if (a == "5") Console.WriteLine("Пятница");
+
+else if (a == "6") Console.WriteLine("Суббота");
+
+else if (a == "7") Console.WriteLine("Воскресенье");
+
+else
+{
+   Console.WriteLine("Такого дня недели нет"); 
+}
