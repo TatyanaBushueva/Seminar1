2aa0147b93d46f4cee68c9422f8ebad734c0a918
Ваше имя английскими буквами <>
2022-12-19 22:33:01 +0300

    Add Task 11 12 14 16

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  11. Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98  */
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 => {number}");
+
+int deleteSecondDigit = DeleteSecondDigit(number);
+Console.WriteLine($"Искомое число => {deleteSecondDigit}");
+
+int DeleteSecondDigit(int num)  //num = number
+{
+    int firstDigit = num / 100;
+    int thirDigit = num % 10;
+    int result = firstDigit * 10 + thirDigit;
+    return result;
+}
+
---
+++ Task12/Program.cs
@@ -0,0 +1,48 @@
+﻿/* 12. Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли первое число
+кратным второму. Если число 1 не кратно числу 2, то
+программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно */
+
+
+/* Console.WriteLine("Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//int res = MultipleDigit(firstNumber, secondNumber);
+
+Console.Write($"{firstNumber}, {secondNumber} => ");
+Console.WriteLine(MultipleDigit(firstNumber, secondNumber));
+
+string MultipleDigit(int firstNum, int secondNum)  //num = number
+{
+    if(firstNum < secondNum)
+    {
+        return "первое меньше второго - проверить кратность невозможно";
+    }
+    else
+    {
+    return firstNum % secondNum == 0 ? "кратно" : $"не кратно, остаток {firstNumber}, остаток {firstNum % secondNum}";
+    }
+}  */
+
+
+Console.WriteLine("Введите первое число");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int res = Multiplicity(numberOne, numberTwo);
+Console.WriteLine(res == 0 ? "Кратно" : $"не кратно, остаток от деления {res}");
+//     Console.WriteLine("Кратно");
+// }
+// else
+// {
+//     Console.WriteLine($"Не кратно, остаток {div}");
+// }
+
+int Multiplicity(int numOne, int numTwo)
+{
+    return numOne % numTwo;
+}
