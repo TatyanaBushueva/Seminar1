85405017a1f94c1f079a87dd0a593d4d2f4e55fd
Ваше имя английскими буквами <>
2022-12-28 23:51:44 +0300

    Добавили задачи 17 18 20 22 семинар 3

---
+++ Task17/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, котораяя 
+//1.принимает на вход координаты точки (X и Y)
+// 2.причем X не равен 0 и Y не равен 0 и 
+//3.выдает номер четрерти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0
+                ? $"Указаные координаты соответствуют четверти -> {quarter}"
+                : "Введены некоректные координаты";
+Console.WriteLine(result);
+
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
+    return 0;
+}
--- Task18/Program.cs
+++ Task18/Program.cs
@@ -1,49 +1,18 @@
 ﻿// Задача 20: Напишите программу, которая
-// принимает на вход координаты двух точек и
-// находит расстояние между ними в 2D
-// пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
+// по заданному номеру четверти, показывает диапозон
+// возможных координат точек в этой четверти (x и y).
 
-System.Console.WriteLine("Введите 1 координ");
-double x1 = Convert.ToInt32(System.Console.ReadLine());
-double y1 = Convert.ToInt32(System.Console.ReadLine());
+Console.WriteLine("Введите номер четверти: ");
 
-System.Console.WriteLine("Введите 2 координ");
-double x2 = Convert.ToInt32(System.Console.ReadLine());
-double y2 = Convert.ToInt32(System.Console.ReadLine());
+string quarter = Console.ReadLine();
+string result = QuarterCoordinates(quarter);
+Console.WriteLine(result);
 
-double res = Hypotenuse(x1,y1,x2,y2);
-System.Console.WriteLine(Math.Round(res,2,MidpointRounding.ToZero) );
-
-double Hypotenuse(double xc1,double yc1,double xc2,double yc2 )
+string QuarterCoordinates(string number)
 {
-
-double pointX = Math.Pow(xc2 - xc1,2);
-double pointY = Math.Pow(yc2 - yc1,2);
-double res = Math.Sqrt(pointX + pointY);
-
-return res;
+    if (number == "1") return "x > 0; y > 0";
+    if (number == "2") return "x < 0; y > 0";
+    if (number == "3") return "x < 0; y < 0";
+    if (number == "4") return "x > 0; y < 0";
+    return "Некоректные данные";
 }
-
-
-
-// Console.WriteLine("Please, enter coordinates of two segments : ");
-// Console.WriteLine("XA : ");
-// int xa = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("XB : ");
-// int xb = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("YA : ");
-// int ya = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("YB : ");
-// int yb = Convert.ToInt32(Console.ReadLine());
-
-// double ourDistance = Math.Round(Distance(xa, xb, ya, yb), 2, MidpointRounding.ToZero);
-// Console.WriteLine($"Distance beteen segments with coordinates {xa} , {xb} and {ya}, {yb} is {ourDistance}.");
-
-
-// double Distance (int x1, int x2, int y1, int y2)
-// {
-//     double distance = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1),2));
-//     return distance;
-// }
